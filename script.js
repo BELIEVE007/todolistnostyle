bf2c13338cb634b38aa19985db4df04ab4e7722e
MAYANk_kAUSHIK <68295643+BELIEVE007@users.noreply.github.com>
2023-06-02 10:06:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,3 +45,139 @@ function addListAfterKeypress(event) {
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+
+
+
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const CharacterDetails = ({ match }) => {
+  const [character, setCharacter] = useState(null);
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacter = async () => {
+      try {
+        const response = await axios.get(`https://swapi.dev/api/people/${match.params.characterId}`);
+        setCharacter(response.data);
+        const filmRequests = response.data.films.map(filmUrl => axios.get(filmUrl));
+        const filmResponses = await Promise.all(filmRequests);
+        const filmData = filmResponses.map(response => response.data);
+        setFilms(filmData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCharacter();
+  }, [match.params.characterId]);
+
+  if (!character) {
+    return <div>Loading character details...</div>;
+  }
+
+  return (
+    <div>
+      <h2>{character.name}</h2>
+      <p>Height: {character.height}</p>
+      <p>Mass: {character.mass}</p>
+      <h3>Films</h3>
+      <ul>
+        {films.map((film, index) => (
+          <FilmLink key={index} film={film} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const FilmLink = ({ film }) => {
+  return (
+    <li>{film.title}</li>
+  );
+};
+
+export default CharacterDetails;
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const FilmDetails = ({ match }) => {
+  const [film, setFilm] = useState(null);
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/films/${match.params.filmId}`)
+      .then(response => setFilm(response.data))
+      .catch(error => console.error(error));
+  }, [match.params.filmId]);
+
+  if (!film) {
+    return <div>Loading film details...</div>;
+  }
+
+  return (
+    <div>
+      <h2>{film.title}</h2>
+      <p>Director: {film.director}</p>
+      <p>Producer: {film.producer}</p>
+      <h3>Characters</h3>
+      <ul>
+        {film.characters.map((character, index) => (
+          <li key={index}>
+            <Link to={`/characters/${index}`}>{character}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FilmDetails;
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+const Home = () => {
+    const [films, setFilms] = useState([]);
+  
+    useEffect(() => {
+      axios.get('https://swapi.dev/api/people')
+        .then(response => setFilms(response.data.results))
+        .catch(error => console.error(error));
+    }, []);
+  
+    return (
+      <div>
+        <h1>Star Wars Films</h1>
+        <ul>
+          {films.map((film, index) => (
+            <li key={index}>
+              <Link to={`/films/${index}`}>{film.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  export default Home;
+  
+  import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import FilmDetails from './FilmDetails';
+import CharacterDetails from './CharacterDetails';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/films/:filmId" element={<FilmDetails />} />
+        <Route path="/characters/:characterId" element={<CharacterDetails />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
